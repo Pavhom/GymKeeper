787ad142309f2179ee0ade560583518a952dbdfd
Pavhom <pav.maven@gmail.com>
2022-07-14 21:27:40 +0300

    added and commented pagination

--- mainscreen/forms.py
+++ mainscreen/forms.py
@@ -1,10 +1,18 @@
-from django import forms
+# from django import forms
 from .models import Post
+from django.forms import TextInput, NumberInput, ModelForm
 
 
-class PostForm(forms.ModelForm):
+class PostForm(ModelForm):
 
     class Meta:
         model = Post
         fields = ('exercise', 'sets_count', 'reps_count', 'weight')
+
+        # widgets = {
+        #     "exercise": TextInput(attrs={'placeholder': 'Exercise'}),
+        #     "sets_count": NumberInput(attrs={'placeholder': 'Sets count'}),
+        #     "reps_count": NumberInput(attrs={'placeholder': 'Reps count'}),
+        #     "weight": NumberInput(attrs={'placeholder': 'Weight'}),
+        # }
 
--- mainscreen/views.py
+++ mainscreen/views.py
@@ -4,11 +4,24 @@ from .models import Post
 from django.http import HttpResponse
 from .forms import PostForm
 from datetime import date
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=date.today()).order_by('-created_date')
+    # paginator = Paginator(posts, 3)  # 3 posts in each page
+    # page = request.GET.get('page')
+    # try:
+    #     posts = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # If page is not an integer deliver the first page
+    #     posts = paginator.page(1)
+    # except EmptyPage:
+    #     # If page is out of range deliver last page of results
+    #     posts = paginator.page(paginator.num_pages)
+    # need add 'page': page to dict, and use {% include "mainscreen/pagination.html" with page=posts %} after
+    # first endfor. Pagination page need only for this func
     return render(request, 'mainscreen/post_list.html', {'posts': posts})
 
 def training_detail(request, pk):
