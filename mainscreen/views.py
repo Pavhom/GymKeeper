8aa2e13fcd038aeb1e46bdbc470014520c6d773e
Pavhom <pav.maven@gmail.com>
2022-07-14 21:56:51 +0300

    added and commented pagination

--- mainscreen/views.py
+++ mainscreen/views.py
@@ -10,19 +10,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=date.today()).order_by('-created_date')
-    # paginator = Paginator(posts, 3)  # 3 posts in each page
-    # page = request.GET.get('page')
-    # try:
-    #     posts = paginator.page(page)
-    # except PageNotAnInteger:
-    #     # If page is not an integer deliver the first page
-    #     posts = paginator.page(1)
-    # except EmptyPage:
-    #     # If page is out of range deliver last page of results
-    #     posts = paginator.page(paginator.num_pages)
+    paginator = Paginator(posts, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     # need add 'page': page to dict, and use {% include "mainscreen/pagination.html" with page=posts %} after
     # first endfor. Pagination page need only for this func
-    return render(request, 'mainscreen/post_list.html', {'posts': posts})
+    return render(request, 'mainscreen/post_list.html', {'page': page, 'posts': posts})
 
 def training_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
