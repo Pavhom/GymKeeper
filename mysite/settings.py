0e926395a9d20d5483f5756fc115c6d5ec27cdf8
Pavhom <pav.maven@gmail.com>
2022-07-29 00:39:54 +0300

    pagination added

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,8 +51,6 @@ INSTALLED_APPS = [
 LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000'
 LOGIN_URL = 'http://127.0.0.1:8000/login/'
 # LOGOUT_REDIRECT_URL = ''
-
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -147,3 +146,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
